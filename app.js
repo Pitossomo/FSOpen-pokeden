f7e362aa531c87e7759b68d4c03f6863fd1fd75d
pitossomo <pitossomo@gmail.com>
2022-06-18 22:02:11 -0300

    Added variables in discord messages

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('health ok')
+})
+
 app.get('/version', (req, res) => {
   // eslint-disable-next-line no-console
   console.log(req)
